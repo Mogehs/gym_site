4948af2028993dbdf9c0a7cd16d2128ca883a4ad
Mogehs <mugheesurrehman06@gmail.com>
2025-01-13 16:39:02 +0500

    updated models and apis

--- backend/controllers/subscriptionController.js
+++ backend/controllers/subscriptionController.js
@@ -35,7 +35,6 @@ export const createSubscription = async (req, res) => {
       endDate,
       status,
       courses: courses,
-      student: req.id,
     });
 
     await subscription.save();
--- backend/models/subscriptionSchema.js
+++ backend/models/subscriptionSchema.js
@@ -6,10 +6,7 @@ const subscriptionSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
-    student: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-    }, //is koo remover kerna hai iski zaroort nhi hai
+
     courses: [
       {
         type: mongoose.Schema.Types.ObjectId,
@@ -19,7 +16,6 @@ const subscriptionSchema = new mongoose.Schema(
     price: { type: Number, required: true },
     startDate: { type: Date, required: true },
     endDate: { type: Date, required: true },
-    //period time add kerna hai start or end date subscription k uper depend kerta hai
     status: { type: String, enum: ["active", "expired"], default: "active" },
   },
   { timestamps: true }
